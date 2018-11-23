7d5981764c3d406e6946ed4208b12cdb25dfc7e2
JJ <2016developer@naver.com>
2018-11-23 23:44:30 +0900

    Rogue11

---
+++ UnityRogueLikeFrame-master/UnityRogueLikeFrame-master/RogueLikeFrame/Assets/01. Scripts/MainGame/MapTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTile : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    // Interface
+
+    bool _canMove = true;
+
+    public void Init(Sprite sprite, float x, float y)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+        gameObject.transform.localPosition = new Vector2(x, y);
+    }
+
+    public bool CanMove()
+    {
+        return _canMove;
+    }
+
+    public void SetMove(bool canMove)
+    {
+        _canMove = canMove;
+    }
+}
---
+++ UnityRogueLikeFrame-master/UnityRogueLikeFrame-master/RogueLikeFrame/Assets/01. Scripts/MainGame/TileMap.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    [SerializeField] GameObject _tileObjectPrefabs;
+
+    void Start ()
+    {
+        //Create();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+
+    // TileMap
+
+    MapTile[,] _mapTileArray;
+
+    // refactoring : 중복 코드 제거
+    public void Create()
+    {
+        Sprite[] spriteList = Resources.LoadAll<Sprite>("Map/MapSprite");
+        
+        // 1층
+        int width = 32;
+        int height = 16;
+
+        _mapTileArray = new MapTile[height, width];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject tileObject = GameObject.Instantiate(_tileObjectPrefabs);
+                tileObject.transform.SetParent(transform);
+                tileObject.transform.localScale = Vector3.one;
+                tileObject.transform.localPosition = Vector3.zero;
+
+                int spriteIndex = 97;
+                if (spriteIndex < spriteList.Length)
+                {
+                    MapTile mapTile = tileObject.GetComponent<MapTile>();
+                    mapTile.Init(spriteList[spriteIndex], x - (width / 2), -y + (height / 2));
+
+                    _mapTileArray[y, x] = mapTile;
+                }
+            }
+        }
+
+        // 2층
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int isCreate = Random.Range(0, 100);
+                if(isCreate < 30)
+                {
+                    GameObject tileObject = GameObject.Instantiate(_tileObjectPrefabs);
+                    tileObject.transform.SetParent(transform);
+                    tileObject.transform.localScale = Vector3.one;
+                    tileObject.transform.localPosition = Vector3.zero;
+
+                    int spriteIndex = 164;
+                    if (spriteIndex < spriteList.Length)
+                    {
+                        MapTile mapTile = tileObject.GetComponent<MapTile>();
+                        mapTile.Init(spriteList[spriteIndex], x - (width / 2), -y + (height / 2));
+                    }
+
+                    GetMapTile(x, y).SetMove(false);
+                }
+            }
+        }
+    }
+
+    MapTile GetMapTile(int x, int y)
+    {
+        return _mapTileArray[y, x];
+    }
+
+    public bool CanMove(int x, int y)
+    {
+        return GetMapTile(x, y).CanMove();
+    }
+
+
+    // Map Objects
+
+    public void SetPlayer(int x, int y, Player player)
+    {
+        MapTile mapTile = GetMapTile(x, y);
+        player.transform.position = mapTile.transform.position;
+    }
+}
